630e8846d6e9ab8146a1c55c815abe6f62078f93
George Zihao Chen <arbiter1333@gmail.com>
2026-06-14 13:41:14 +1000

    Add: combination-target-sum-ii - submission-10

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-10.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> res = {};
+
+        sort(candidates.begin(), candidates.end());
+
+        vector<int> rec_res = {};
+        recurse(candidates, target, 0, rec_res, res);
+
+        return res;
+    }
+
+    void recurse(const vector<int>& nums, const int target, int start, vector<int>& rec_res, vector<vector<int>>& res) {
+        if (target == 0) {
+            res.push_back(rec_res);
+            return;
+        } else if (target <= 0 || start >= nums.size()) {
+            return;
+        }
+
+        /*
+        for (int j = start; j < nums.size(); j++) {
+            // cout << "start = " << start << " new_target = " << new_target << " and we are trying " << nums[j] << '\n';
+            recurse(nums, new_target, j, rec_res, res);
+        }
+        */
+
+        rec_res.push_back(nums[start]);
+        recurse(nums, target - nums[start], start + 1, rec_res, res);
+        rec_res.pop_back();
+
+        int i = 1;
+        while (start + i < nums.size() && nums[start + i] == nums[start]) {
+            i++;
+        }
+        recurse(nums, target, start + i, rec_res, res);
+    }
+};
