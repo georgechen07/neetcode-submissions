f3efadf5ebbe54cf55a44106eaa246e3b1c67dfc
George Zihao Chen <arbiter1333@gmail.com>
2026-06-26 01:45:11 +1000

    Add: set-zeroes-in-matrix - submission-9

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-9.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        bool top = false;
+        bool left = false;
+        
+        for (int i = 0; i < matrix.size(); i++) {
+            if (matrix[i][0] == 0) {
+                left = true;
+                break;
+            }
+        }
+
+        for (int i = 0; i < matrix[0].size(); i++) {
+            if (matrix[0][i] == 0) {
+                top = true;
+                break;
+            }
+        }
+        
+        for (int i = 1; i < matrix.size(); i++) {
+            for (int j = 1; j < matrix[0].size(); j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < matrix.size(); i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < matrix[0].size(); j++) {
+                    matrix[i][j] = 0;
+                    // cout << "i = " << i << " j = " << j << '\n';
+                }
+            }
+        }
+
+        for (int i = 1; i < matrix[0].size(); i++) {
+            if (matrix[0][i] == 0) {
+                for (int j = 1; j < matrix.size(); j++) {
+                    matrix[j][i] = 0;
+                    cout << "i = " << i << " j = " << j << '\n';
+                }
+            }
+        }
+
+        if (top == true) {
+            for (int i = 0; i < matrix[0].size(); i++) {
+                matrix[0][i] = 0;
+            }
+        }
+
+        if (left == true) {
+            for (int i = 0; i < matrix.size(); i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
