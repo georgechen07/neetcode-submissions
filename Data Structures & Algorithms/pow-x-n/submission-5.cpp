9b2ff6fbbf6ae8fa33f0188f7f8f2821a7151936
George Zihao Chen <arbiter1333@gmail.com>
2026-06-29 18:54:25 +1000

    Add: pow-x-n - submission-5

---
+++ Data Structures & Algorithms/pow-x-n/submission-5.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 1 || x == 0) {
+            return x;
+        }
+        if (n == 0) {
+            return 1;
+        }
+        return recurse(x, n);
+    }
+
+    double recurse(double const x, int n) {
+        if (n == 1) {
+            return x;
+        }
+
+        if (n < 0) {
+            return 1 / recurse(x, -n);
+        }
+
+        if (n % 3 == 0) {
+            auto curr = recurse(x, n / 3);
+            return curr * curr * curr;
+        } else if (n % 2 != 0) {
+            return x * recurse(x, n - 1);
+        } else {
+            auto curr = recurse(x, n / 2);
+            return curr * curr;
+        }
+    }
+};
