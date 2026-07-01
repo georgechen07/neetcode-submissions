6f1c6314c76ae7026f0d7bcb018e999650aec35b
George Zihao Chen <arbiter1333@gmail.com>
2026-07-01 22:29:04 +1000

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        auto l = s.begin();
+        auto r = s.begin();
+        unordered_map<char, int> keyT;
+        for (auto const& c : t) {
+            keyT[c]++;
+        }
+
+        unordered_map<char, int> keyS;
+        while (r - l < t.size()) {
+            keyS[*r]++;
+            r++;
+        }
+
+        string_view res = "";
+        while (l < s.end()) {
+            if (helper(keyS, keyT)) {
+                if (r - l < res.size() || res == "") {
+                    res = string_view(l, r);
+                }
+                // cout << res << '\n';
+                keyS[*l]--;
+                l++;
+            } else if (r < s.end()) {
+                keyS[*r]++;
+                r++;
+                cout << *r << "map: \n";
+                for (const auto [key, element] : keyS) {
+                    cout << key << " " << element << '\n';
+                }
+                cout << helper(keyS, keyT) << '\n';
+            } else {
+                keyS[*l]--;
+                l++;
+            }
+        }
+
+        return static_cast<string>(res);
+    }
+
+    bool helper(unordered_map<char, int> keyS, unordered_map<char, int> keyT) {
+        for (const auto [key, element] : keyT) {
+            if (element > keyS[key]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
