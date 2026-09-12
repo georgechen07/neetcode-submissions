7f26837bfeea4108c6339f66af67e1382515f355
George Chen <arbiter1333@gmail.com>
2026-09-12 22:30:30 +1000

    Add: concatenation-of-array - submission-4

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-4.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        auto res = vector<int>(2 * nums.size());
+        for (int i = 0; i < nums.size(); ++i) {
+            res[i] = nums[i];
+            res[nums.size() + i] = nums[i];
+        }
+
+        return res;
+    }
+};
