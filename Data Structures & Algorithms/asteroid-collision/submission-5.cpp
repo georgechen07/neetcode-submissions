73cbb5399040689243ca6bef5e912a3a58b5dbe0
George Chen <arbiter1333@gmail.com>
2026-09-17 20:28:49 +1000

    Add: asteroid-collision - submission-5

---
+++ Data Structures & Algorithms/asteroid-collision/submission-5.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> s;
+        for (auto ast : asteroids) {
+            while (ast < 0 && !s.empty()) {
+                if (s.back() > 0) {
+                    if (s.back() + ast == 0) {
+                        s.pop_back();
+                        ast = 0;
+                    } else if (s.back() + ast < 0) {
+                        s.pop_back();
+                    } else {
+                        ast = 0;
+                    }
+                } else {
+                    break;
+                }
+            }
+            if (ast != 0) {
+                s.push_back(ast);
+            }
+        }
+
+        // vector<int> res(s.size());
+        // int i = 1;
+        // while (!s.empty()) {
+        //     res[res.size() - i] = s.top();
+        //     s.pop();
+        //     ++i;
+        // }
+
+        return s;
+    }
+};
